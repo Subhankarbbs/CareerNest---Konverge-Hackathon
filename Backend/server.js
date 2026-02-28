a9f43f8ba33f5f736ea6efbd5442955004145d20
Subhankar Sahoo <subhankar2@outlook.com>
2026-02-28 14:08:18 +0530

    Deployment fixes

--- Backend/server.js
+++ Backend/server.js
@@ -39,7 +39,7 @@ app.use("/api/jobs", jobRoutes);
 //   res.status(500).json({ message: "Something went wrong" });
 // });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000", // backend
+        target: "https://careernest-backend-z7pa.onrender.com/", // backend
         changeOrigin: true,
       },
     }
